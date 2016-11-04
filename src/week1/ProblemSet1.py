e195faa609d8e408d7d6f60f96b425d6ddea519d
Bhabani <bhabanism@outlook.com>
2016-11-04 20:39:41 +0530

    #2 week1 inprog...

--- src/week1/ProblemSet1.py
+++ src/week1/ProblemSet1.py
@@ -104,7 +104,8 @@ auto-grader may not give you credit.
 """
 #%%
 def problem1_4(miles):
-    pass # replace this pass (a do-nothing) statement with your code
+    feet = miles * 5280
+    print("There are {} feet in {} miles.".format(feet,miles));
 
 
     
